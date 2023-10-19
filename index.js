d5d346617c5f79ef7a61015300865d08e9b1d1fe
Nicholas Otieno <niklasoti@gmail.com>
2023-10-19 12:12:19 +0200

    Updates ⚡️

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ import logger from "koa-logger";
 import serve from "koa-static";
 import { koaBody } from "koa-body";
 import "dotenv/config.js";
-import { homeRouter, aboutRouter } from "./router/index.js";
+import { homeRouter, aboutRouter, hookRouter } from "./router/index.js";
 import notFound from "./middlewares/notFound.js";
 import errorHandler from "./middlewares/errorHandler.js";
 import security from "./middlewares/security.js";
 import views from "./middlewares/views.js";
+// import { rateLimiter } from "./middlewares/limiter.js";
 
 const app = new koa();
+const PORT = process.env.PORT || 3000;
 
 app
+  // .use(rateLimiter)
   .use(serve("./public"))
   .use(security)
   .use(koaBody())
@@ -21,8 +24,10 @@ app
   .use(homeRouter.allowedMethods())
   .use(aboutRouter.routes())
   .use(aboutRouter.allowedMethods())
+  .use(hookRouter.routes())
+  .use(hookRouter.allowedMethods())
   .use(notFound)
   .on("error", errorHandler)
-  .listen(3000, () => {
-    console.log("Listening on port 3000");
+  .listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -1,8 +1,9 @@
-import { RateLimit } from "../deps.js";
+import RateLimit from "koa2-ratelimit";
 
 const rateLimit = RateLimit.RateLimit;
 
-export const limiter = rateLimit.middleware({
+// Find the best strategy later.
+export const rateLimiter = rateLimit.middleware({
   interval: { min: 15 }, // 15 minutes = 15*60*1000
-  max: 100, // limit each IP to 100 requests per interval
+  max: 5000, // Limit each IP to 500 requests per interval
 });
